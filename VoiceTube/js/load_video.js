3370e062b545738f425f593a16e32dc618dd8cd4
john970473 <john970473@gmail.com>
2018-04-25 22:38:16 +0800

    change line

--- VoiceTube/js/load_video.js
+++ VoiceTube/js/load_video.js
@@ -1,16 +1,16 @@
 
 $(function () {
 	//
-	var video_url = Array("oWP9Riq-ZBg", "9bAiXJoNdy0" ,"A_DRNbpsU3Q" ,"ivUOBdHu6VE" ,"jKTZAsDVkzs" ,"uSTNyHkde08"); 
+	var video_url = Array("oWP9Riq-ZBg", "9bAiXJoNdy0" ,"A_DRNbpsU3Q" ,"ivUOBdHu6VE" ,"jKTZAsDVkzs" ,"uSTNyHkde08");
 	var video_title = Array("How to introduce yourself", "One thing that makes you a better friend",
-	 "Why is the 'Mona Lisa' so famous", "Impractical Jokers - Joe the Genie(Punishment)", 
+	 "Why is the 'Mona Lisa' so famous", "Impractical Jokers - Joe the Genie(Punishment)",
 	 "What it means to be pretty?", "Why isn\'t the world covered in poop?")
 	for(let i=0; i<6; i++){
 		let html="";
 		html += "<div class=\"col-xs-12 col-sm-6 col-md-4 col-lg-3\">";
 		html += "<div class=\"thumbnail\">"
-		html += "<div><a href=\"https://john970473.github.io/"+video_url[i]+"/\"><img src=\"http://img.youtube.com/vi/"+video_url[i]+"/sddefault.jpg\"></a></div>";
-		html += "<div><a href=\"https://john970473.github.io/"+video_url[i]+"\" title=\"title\">"+video_title[i]+"</a></div>";
+		html += "<div><a href=\"https://john970473.github.io/VT/"+video_url[i]+"/\"><img src=\"http://img.youtube.com/vi/"+video_url[i]+"/sddefault.jpg\"></a></div>";
+		html += "<div><a href=\"https://john970473.github.io/VT/"+video_url[i]+"\" title=\"title\">"+video_title[i]+"</a></div>";
 		html += "<div ><span class=\"label label-success\">初級</span><span class=\"label label-default\">中文</span><span class=\"label label-warning\">美國腔</span></div>";
 		html += "</div>";
 		html += "</div>";
